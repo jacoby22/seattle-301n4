f0eee1845181a85f1bead5aa06b9c742e66ff563
Nick Jacoby <nickjacoby22@gmail.com>
2016-08-30 19:15:41 -0700

    all comments added

--- class-14-managing-state/pair-assignment/starter-code/scripts/articleController.js
+++ class-14-managing-state/pair-assignment/starter-code/scripts/articleController.js
@@ -8,6 +8,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // RESPONSE: First, a variable is declared (articleData), which stores an anonymous function reference. The anonymous function takes one parameter called article, stores that article parameter value as an array of articles through the ctx.articles property. The next() method call will call the "next" function chained, whatever that function may be, in the page() call in routes.js. The Article.findWhere method call takes the 'id' value sought from the HTML5 database and compares it to the ctx.params.id parameter, which is the comparison for matching with 'id'. The articleData function is called as a callback once the webDB execute request is sent. Overall, the method filters articles by id.
   articlesController.loadById = function(ctx, next) {
     var articleData = function(article) {
       ctx.articles = article;
@@ -18,6 +19,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // RESPONSE: Similar to the articlesController.loadById method, this method filters the articles by author name and passes that to the initIndex method call to render the filtered author names on the DOM. The .replace() method replaces the '+' with empty spaces due to the fact that the keys needed the concatenated '+' to be permissible and now, we need to match the exact author name with what is compared in our filter.
   articlesController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
@@ -28,6 +30,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // RESPONSE: Again, this method call is similar to both the articlesController.loadById method and the articlesController.loadByAuthor method. Instead, this method filters article content by category. It waits for when a route is fired (change on the category filter) and runs the chain of methods via the routes.js file.
   articlesController.loadByCategory = function(ctx, next) {
     var categoryData = function(articlesInCategory) {
       ctx.articles = articlesInCategory;
@@ -38,6 +41,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // RESPONSE: This method again, utilizes the context object passed in from page.js. It also, has a similar variable-anonymous function pair for populating the context object's .articles property full, in this case, of all the article data. First, though, we check to see if the Article.all array has any values in it (aka has been populated). If it does, we set those values equal to Article.all, otherwise, we fetch all the articles with Article.fetchAll and store them in ctx.articles using articleData as our callback. Overall, this method populates our context object with all the articles in the database.
   articlesController.loadAll = function(ctx, next) {
     var articleData = function(allArticles) {
       ctx.articles = Article.all;
--- class-14-managing-state/pair-assignment/starter-code/scripts/articleView.js
+++ class-14-managing-state/pair-assignment/starter-code/scripts/articleView.js
@@ -13,6 +13,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // RESPONSE: This method populates both the authors filter and the category filter. First, this method creates a variable reference to the handlebars.js template with an id of '#option-template'. The options variable is then assigned a mapped array of template objects with 'val' and 'author' key value pairs. The mapping process eliminates duplicate authors, we can append to the #author-filter afterwards. The categories filter is constructed differently because it depends on SQL data rather than data on the DOM. It calls the Article.allCategories method, taking in an anonymous function as its callback to the allCategories method. the rows parameter represents the DISTINCT data returned by the SQL request. Once we check to see that the filters have yet to be populated, we append the unique categories to the filter, this time, by mapping inside of the jQuery .append() method call.
   articleView.populateFilters = function() {
     var options,
       template = Handlebars.compile($('#option-template').text());
@@ -20,9 +21,9 @@
     // Example of using model method with FP, synchronous approach:
     // NB: This method is dependant on info being in the DOM. Only authors of shown articles are loaded.
     options = Article.allAuthors().map(function(author) { return template({val: author}); });
-    if ($('#author-filter option').length < 2) { // Prevent duplication
-      $('#author-filter').append(options);
-    };
+    // if ($('#author-filter option').length < 2) { // Prevent duplication
+    $('#author-filter').append(options);
+    // };
 
     // Example of using model method with async, SQL-based approach:
     // This approach is DOM-independent, since it reads from the DB directly.
@@ -38,6 +39,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // RESPONSE: This method handles the event of a user selecting an item from either filer. In this case, the execution path is heavily dependent on routes.js and the page.js wrapper. We start by targeting an HTML element with the id of filters and place a .one event handler on the tag to listen for a 'change' event. We then delegate the values of the 'select' tag within the '#filters' element. When a change event is fired, the variable resource stores the string 'author' or 'category' depending on the id of the element that was selected. Then, we call the page function, passing in a concatenated string with the following: the file path starting with 'author' or 'category', the value of the category or author filter item selected with white space replaced with '+'. The page function references the appropriate route from routes.js from the resource value and displays the DOM accordingly based on the order of functions specified by routes.js.
   articleView.handleFilters = function() {
     $('#filters').one('change', 'select', function() {
       resource = this.id.replace('-filter', '');
@@ -124,6 +126,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // RESPONSE: This method hides and shows article content based on previous filters selected and the current state of the articles array passed in as a parameter. We start by showing the DOM element with id of articles. We then hide its siblings. Article elements within #articles are removed and then, we loop over each article from the articles array passed in as a parameter to decipher which articles to display. The execution path depends on which route the user takes. For instance, if the user filters by author, the articles array is populated with the correct results and the articleView.index method renders only those results. This method depends on the chaining of previous methods in routes.js for its articles content. The last bit of code only shows the teaser amount for each article result displayed.
   articleView.index = function(articles) {
     $('#articles').show().siblings().hide();
 
@@ -134,6 +137,7 @@
 
     articleView.populateFilters();
     // COMMENT: What does this method do?  What is it's execution path?
+    // RESPONSE: See description above for article.handleFilters method purpose and execution path.
     articleView.handleFilters();
 
     // DONE: Replace setTeasers with just the truncation logic, if needed:
